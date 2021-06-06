0ae371415b76a5faf6d25453f0ce61664da0f5da
Your Name <you@example.com>
2021-06-06 18:36:45 +0530

    commiting home.ejs and post.js

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 module.exports.home = function(req,res){
     console.log(req.cookies);
-    res.cookie('User_id',238940);
+    // res.cookie('User_id',238940);
     return res.render('home',{
         title: 'home'
     });
