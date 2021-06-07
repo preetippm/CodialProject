200de935b26ddd3b19cda438113fefd81ee640f1
Your Name <you@example.com>
2021-06-08 03:53:23 +0530

    creating schema for comments

--- models/post.js
+++ models/post.js
@@ -8,7 +8,16 @@ const postSchema = new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
-    }
+    },
+
+    //include the array of ids of all comments in this post schema itself
+    comment: 
+    [
+        {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'comment'
+        }
+    ]
     },{
         timestamps:true
     });
