6c89575bd23d73f92164ffc5d654beaa8c7abe59
Your Name <you@example.com>
2021-06-07 19:02:06 +0530

    display posts and related users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,22 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    // res.cookie('User_id',238940);
-    return res.render('home',{
-        title: 'home'
+    // console.log(req.cookies);
+    // // res.cookie('User_id',238940);
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title: 'Codial | Home',
+    //         posts:posts
+    // })
+    
+    // });
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+                    title: 'Codial | Home',
+                    posts:posts
     });
 
+})
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,7 +3,7 @@ const Post = require('../models/post');
 module.exports.create = function(req,res){
     Post.create({
         content : req.body.content,   //schema name : form content //textArea
-        user:req.user._id
+        user:req.user
     },function(err,post){
         if(err){
             console.log('error is creating the post');
