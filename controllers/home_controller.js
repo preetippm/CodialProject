f634d81969f6e7194d14cf736fd81f6a69f31dcf
Your Name <you@example.com>
2021-06-09 22:15:16 +0530

    showing comments with the posts with the user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -12,11 +12,27 @@ module.exports.home = function(req,res){
     
     // });
 
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home',{
                     title: 'Codial | Home',
                     posts:posts
     });
 
 })
+
+// Post.find({}).populate('comment').exec(function(err,posts){
+//     return res.render('home',{
+//                 title: 'Codial | Home',
+//                 posts:posts
+// });
+
+// })
 }
--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,12 @@
-const mongoose = require('../config/mongoose');
+const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
     content:{
         type:String,
         require:true
     },
+
+    // comment belongs to a user
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
