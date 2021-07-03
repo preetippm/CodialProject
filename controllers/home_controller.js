ca4e7b355915559c658b5b9694f342c562129097
Your Name <you@example.com>
2021-07-03 23:51:30 +0530

    converting code to Async Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,42 +2,31 @@ const Post = require('../models/post');
 
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
-    // console.log(req.cookies);
-    // // res.cookie('User_id',238940);
-
-    // Post.find({},function(err,posts){
-    //     return res.render('home',{
-    //         title: 'Codial | Home',
-    //         posts:posts
-    // })
-    
-    // });
-
-    Post.find({})
+module.exports.home =  async function(req,res){
+   
+    try{ 
+        //populate the user of each post
+    let posts = await Post.find({})                  //-------------1--await
     .populate('user')
     .populate({
         path:'comments',
         populate:{
             path:'user'
         }
-    })
-     .exec(function(err,posts){
-
-        User.find({}, function(err, users){
-            return res.render('home',{
-                title: 'Codial | Home',
-                posts:posts,
-                all_users:users
-        });      
     });
-})
+     let users = await User.find({});               //--------------2--await
 
-// Post.find({}).populate('comment').exec(function(err,posts){
-//     return res.render('home',{
-//                 title: 'Codial | Home',
-//                 posts:posts
-// });
 
-// })
+        return res.render('home',{
+            title: 'Codial | Home',                 //---------------returns the data to the browser
+            posts:posts,
+            all_users:users     
+    });
+
+
+   } catch(err){
+       console.log('Error ',err);                   //----------------if there is any error
+       return;
+
+   }
 }
