1e68b3fd0a42c853148770f861f45a5198a644bc
Your Name <you@example.com>
2021-07-03 17:14:17 +0530

    SCSS of Home Page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -42,6 +42,18 @@ module.exports.profile=function(req,res){
 
 }
 
+module.exports.update = function(req,res){
+   if(req.user.id == req.params.id){
+      User.findByIdAndUpdate(req.params.id,req.body, function(err, user){
+         return res.redirect('back'); 
+      });
+
+   }else{
+      return res.status(401).send('Unauthorized');
+   }
+
+}
+
 //render the Sign In page
 module.exports.signIn = function(req,res){
    if(req.isAuthenticated()){
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const passport = require('passport');
 const userController = require('../controllers/users_controller');
 
 router.get('/profile/:id',passport.checkAuthentication,userController.profile); // if user found then only can go to profile page
+router.post('/update/:id',passport.checkAuthentication,userController.update);
+
 
 router.get('/sign-in',userController.signIn);
 router.get('/sign-up',userController.signUp);
